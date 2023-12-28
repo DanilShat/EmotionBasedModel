d49afe97c10aa575f14b23036513be68422b8ad0
Daniil Shatilov <127853335+DanilShat@users.noreply.github.com>
2023-12-28 23:40:37 +0200

    Delete EBM directory

--- EBM/__init__.py
+++
@@ -1,5 +0,0 @@
-from .EBTextClassifier import EBTextClassifier
-from .EBTextRegressor import EBTextRegressor
-from .EBTextClustering import EBTextClusterization
-
-__all__ = ['EBTextClassifier', 'EBTextRegressor', 'EBTextClusterization']
