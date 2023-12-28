dbb0bebdf5553e473c16f75dab4e7ef7864b059b
Daniil Shatilov <127853335+DanilShat@users.noreply.github.com>
2023-12-28 23:40:23 +0200

    Create __init__.py

---
+++ EmotionBasedModel/__init__.py
@@ -0,0 +1,5 @@
+from .EBTextClassifier import EBTextClassifier
+from .EBTextRegressor import EBTextRegressor
+from .EBTextClustering import EBTextClusterization
+
+__all__ = ['EBTextClassifier', 'EBTextRegressor', 'EBTextClusterization']
